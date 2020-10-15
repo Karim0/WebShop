9866d3c8b6f066c4e16906de3021758f7ba7ab7d
Karim <karim20.ks@gmail.com>
2020-10-15 13:49:12 +0600

    added new defs in the views.py and new urls

--- shop/urls.py
+++ shop/urls.py
@@ -8,5 +8,9 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.home_page, name='index'),
     path('cart/', views.cart_page, name='cart'),
-    path('about/', views.about_page, name='about')
+    path('about/', views.about_page, name='about'),
+    path('contact/', views.contact_page, name='contact'),
+    path('products/', views.product_page, name='products'),
+    path('products_cat/<int:pk>', views.product_cat_page, name='products'),
+    path('product_detail/<int:pk>', views.product_detail_page, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -29,3 +29,56 @@ def about_page(request):
         'type': 'sub-head'
     }
     return render(request, 'shop/about.html', content)
+
+
+def contact_page(request):
+    content = {
+        'pagename': 'Контакты',
+        'type': 'sub-head'
+    }
+    return render(request, 'shop/contact.html', content)
+
+
+def product_page(request):
+    content = {
+        'pagename': 'Товары',
+        'type': 'sub-head',
+        'products': Product.objects.all()
+    }
+    return render(request, 'shop/product.html', content)
+
+
+def product_detail_page(request, pk):
+    content = {
+        'pagename': 'О товаре',
+        'type': 'sub-head',
+        'product': Product.objects.get(id=pk)
+    }
+    return render(request, 'shop/product_detail.html', content)
+
+
+def product_cat_page(request, pk):
+    content = {
+        'pagename': 'О товаре',
+        'type': 'sub-head',
+        'product': Product.objects.filter(subcategory__in=Subcategory.objects.filter(category_id=pk))
+    }
+    return render(request, 'shop/product.html', content)
+
+
+def product_subcat_page(request, pk):
+    content = {
+        'pagename': 'О товаре',
+        'type': 'sub-head',
+        'product': Product.objects.filter(subcategory_id=pk)
+    }
+    return render(request, 'shop/product.html', content)
+
+
+def filter_prod(cat_id, prop):
+    prods = ProductChar.objects.filter(prod__subcategory__category_id=cat_id)
+
+    for i in prop.keys():
+        prods = prods.filter(prodval__value__contains=prop[i]).distinct()
+
+    return prods
