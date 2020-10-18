50cc40ee5606cf5fb134e42cad680c8e156cf135
Karim <karim20.ks@gmail.com>
2020-10-18 15:14:02 +0600

    search product, order product

--- WebShop/settings.py
+++ WebShop/settings.py
@@ -86,9 +86,18 @@ WSGI_APPLICATION = 'WebShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop',
+        'USER': 'shop',
+        'PASSWORD': 'shop',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Password validation
