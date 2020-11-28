cbac1c3bceb7c65e5001e663dd01c87f7eb6cdc0
root <root@hanok.kz>
2020-11-28 16:49:26 +0600

    dsksdfk

--- shop/templatetags/temp_filter.py
+++ shop/templatetags/temp_filter.py
@@ -26,3 +26,14 @@ def unique(value):
 @register.filter()
 def multiplication(value):
     return value * 10
+
+@register.filter()
+def pagi_cut(value, p):
+    if value.find('?')>=0:
+        pattern = value.split('?')
+        if pattern[1].find('page=')!=-1:
+            return pattern[0]+'?'+'&'.join(pattern[1].split('&')[:-1])+'&page='+str(p)
+        else:
+            return f'{pattern[0]}?{pattern[1]}&page={p}'
+    else:
+        return f'{value}?page={p}'
